2f238a4c87b47499cdc0e11a6f3bc3e5392212f3
Silas Gomez <SilasGomez1223@hotmail.com>
2024-06-12 16:51:13 -0700

    initial commit

---
+++ src/app/external-link-handler/page.tsx
@@ -0,0 +1,11 @@
+import ExternalLinkHandler from "../../components/ExternalLinkHandler";
+
+const ExternalLinkHandlerPage = ({
+  searchParams,
+}: {
+  searchParams: { url: string };
+}) => {
+  return <ExternalLinkHandler searchParams={searchParams} />;
+};
+
+export default ExternalLinkHandlerPage;
